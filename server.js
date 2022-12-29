0b4040583fac27c0224a002ab7c30a2ea661c4e9
lvfd <lyufudi@yahoo.com>
2022-12-29 15:58:05 +0800

    更改为使用express建立server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.get('/', express.static(path.join(__dirname, 'pages')))
+
+app.listen(port, () => {
+  console.log(path.resolve(__dirname, 'pages'))
+  console.log(`Express Server listening on port ${port}`)
+})
